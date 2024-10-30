185a723442419ba8f467676f264cec51008f4ca7
krisave <kavere4@lsu.edu>
2024-10-30 14:40:45 -0500

    Re-add database

--- Dice_Tavern/Program.cs
+++ Dice_Tavern/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddMySqlDataSource(builder.Configuration.GetConnectionString("Default")!);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

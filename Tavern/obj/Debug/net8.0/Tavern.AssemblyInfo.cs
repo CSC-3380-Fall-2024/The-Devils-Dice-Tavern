2a1eff565f592878d7527f0f1a85856531783f63
Gabrielle King <gking16@lsu.edu>
2024-11-13 23:21:25 -0600

    committing files altered during run

--- Tavern/obj/Debug/net8.0/Tavern.AssemblyInfo.cs
+++ Tavern/obj/Debug/net8.0/Tavern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tavern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1290bff18d9bdbfed17c4b3a592790f679b964ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f259197cf1fd9dd3f8bbaee219ec5ae0086af4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tavern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tavern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

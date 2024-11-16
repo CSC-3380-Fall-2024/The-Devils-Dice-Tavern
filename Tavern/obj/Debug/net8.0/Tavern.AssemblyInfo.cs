110385b92cc832e18dfd1402e8f230ac3cdefde3
krisave <kavere4@lsu.edu>
2024-11-15 21:53:19 -0600

    Login Page

--- Tavern/obj/Debug/net8.0/Tavern.AssemblyInfo.cs
+++ Tavern/obj/Debug/net8.0/Tavern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tavern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5536933e9ebe9d56102098bc653a7f38d8def31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1290bff18d9bdbfed17c4b3a592790f679b964ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tavern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tavern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
